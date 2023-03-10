3f494c958e2ee1a758ae7d274a79a3ea87d3ab0e
KIM JIEUN <gch00335@naver.com>
2023-03-10 10:40:30 +0900

    주석문 추가

--- JAVA/src/kr/ac/kopo/day04/OperationMain05.java
+++ JAVA/src/kr/ac/kopo/day04/OperationMain05.java
@@ -4,7 +4,13 @@ public class OperationMain05 {
 
 	public static void main(String[] args) {
 		
-
+		int num = 0x0055ff00;
+		
+		System.out.printf("%x\n", num);
+		// x 가 소문자면 소문자로 , X가 대문자면  0X0055FF00 으로 나옴
+		
+		System.out.printf("%x\n" , ~num);
+		// ~ 이거는 [반대연산자] 
 	}
 
 }
