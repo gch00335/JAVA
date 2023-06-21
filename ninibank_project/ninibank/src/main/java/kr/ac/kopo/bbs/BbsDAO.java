dfa7b085ad98d869cbf723846e6ea485dc60abce
니니찌니 <126840613+gch00335@users.noreply.github.com>
2023-06-21 15:36:02 +0900

    오라클에 맞게 쿼리 수정 DB전송 완
    LIST로 불러오는게 필요

--- ninibank_project/ninibank/src/main/java/kr/ac/kopo/bbs/BbsDAO.java
+++ ninibank_project/ninibank/src/main/java/kr/ac/kopo/bbs/BbsDAO.java
@@ -28,57 +28,57 @@ public class BbsDAO {
 		}
 
 
-	public String getDate() {
-		String SQL = "SELECT NOW()";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				return rs.getString(1);
+	 public String getDate() {
+			String SQL = "SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD') FROM DUAL";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					return rs.getString(1);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			return ""; // 데이터베이스 오류
 		}
-		return ""; // 데이터베이스 오류
-	}
 
-	public int getNext() {
-		String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				return rs.getInt(1) + 1;
+		public int getNext() {
+			String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				rs = pstmt.executeQuery();
+				if (rs.next()) {
+					return rs.getInt(1) + 1;
+				}
+				return 1; // 첫번째 게시물인 경우
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			return 1; // 첫번째 게시물인 경우
-		} catch (Exception e) {
-			e.printStackTrace();
+			return -1; // 데이터베이스 오류
 		}
-		return -1; // 데이터베이스 오류
-	}
 
-	public int write(String bbsTitle, String userID, String bbsContent) {
-		conn = getConnection();
-		String SQL = "INSERT INTO BBS VALUES(?, ?, ?, ?, ?, ?)";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setInt(1, getNext());
-			pstmt.setString(2, bbsTitle);
-			pstmt.setString(3, userID);
-			pstmt.setString(4, getDate());
-			pstmt.setString(5, bbsContent);
-			pstmt.setInt(6, 1);
+		public int write(String bbsTitle, String userID, String bbsContent ) {
+			conn = getConnection();
+			String SQL = "INSERT INTO BBS VALUES(?, ?, ?, ?, ?, ?)";
+			try {
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setInt(1, getNext());
+				pstmt.setString(2, bbsTitle);
+				pstmt.setString(3, userID);
+				pstmt.setString(4, getDate());
+				pstmt.setString(5, bbsContent);
+				pstmt.setInt(6, 1);
 
-			return pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
+				return pstmt.executeUpdate();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return -1; // 데이터베이스 오류
 		}
-		return -1; // 데이터베이스 오류
-	}
-	
+		
 	public ArrayList<Bbs> getList(int pageNumber){
 		conn = getConnection();
-		String SQL = "SELECt * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC FETCH FIRST 10 ROWS ONLY";
 		ArrayList<Bbs> list = new ArrayList<Bbs>();
 		try {
 			PreparedStatement pstmt = conn.prepareStatement(SQL);
